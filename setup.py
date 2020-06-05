f00fa44303ad849cb82490acc57b891246410492
Alan Rubin <alan.rubin@wehi.edu.au>
2020-06-05 22:15:15 +1000

    updated setup metadata

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ setuptools.setup(
     name="hgvsp",
     version="0.4",
     packages=["hgvsp"],
-    url="https://github.com/FowlerLab/hgvs-patterns.git",
-    license="MIT",
-    author="Daniel",
-    author_email="esposito.d@wehi.edu.au",
+    url="https://github.com/VariantEffect/hgvs-patterns.git",
+    author="Daniel Esposito and Alan F Rubin",
+    author_email="alan.rubin@wehi.edu.au",
     description=(
-        "A python utility containing HGVS Regex patterns to "
-        "match a subset of the HGVS standard."
+        "Regular expression-based validation of HGVS variant strings for clinical genetics and genomics applications."
     ),
-    keywords=["hgvs", "variants", "regex", "bioinformatics", "biology"],
     classifiers=[
-        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
     test_suite="tests",
 )
