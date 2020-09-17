494bf9d7ce01650586046b292755a3dbe0d949cb
Alan Rubin <alan.rubin@wehi.edu.au>
2020-09-17 16:05:44 +1000

    move pattern utils

--- mavehgvs/patterns/dna.py
+++ mavehgvs/patterns/dna.py
@@ -1,6 +1,7 @@
 import re
-from typing import Sequence, Optional, Pattern
 from fqfa.constants.iupac.dna import DNA_BASES
+from mavehgvs.patterns.util import combine_patterns, remove_named_groups
+
 
 dna_nt: str = rf"[{''.join(DNA_BASES)}]"
 """str: Pattern matching any uppercase base.
@@ -72,52 +73,6 @@ delins_tx: str = rf"(?P<delins>(?:(?P<start>{pos_tx})_(?P<end>{pos_tx})delins)|(
 """
 
 
-def combine_patterns(
-    patterns: Sequence[str], groupname: Optional[str] = None
-) -> Pattern:
-    """Combine multiple pattern strings and generate a compiled regular expression object.
-
-    Because multiple identical group names are not allowed in a pattern, the resulting object renames all named match
-    groups such they are prefixed with the first match group name in the pattern. For example,
-    :code:`(?P<substitution>(?P<position>[1-9][0-9]*)...` becomes
-    :code:`(?P<substitution>(?P<substitution_position>[1-9][0-9]*)...`.
-
-    The function assumes that all input patterns are enclosed in parentheses.
-
-    Parameters
-    ----------
-    patterns : Sequence[str]
-        Sequence of pattern strings to combine.
-
-    groupname : Optional[str]
-        Name for the capture group surrounding the resulting pattern. If this is None, a non-capturing group will be
-        used instead.
-
-    Returns
-    -------
-    re.Pattern
-        Compiled regular expression that matches any of the input patterns. Match groups are renamed as described above
-        to attempt to ensure uniqueness across the combined pattern.
-
-    """
-    tag_re = re.compile(r"\(\?P<(\w+)>")
-    stripped_patterns = list()
-    for p in patterns:
-        tags = list(tag_re.finditer(p))
-        prefix = f"{tags[0].group(1)}_"
-        new_p = p
-        for t in tags[:0:-1]:
-            start, end = t.span(1)
-            new_p = "".join((new_p[:start], prefix, new_p[start:]))
-        stripped_patterns.append(new_p)
-    if groupname is None:
-        combined = rf"(?:{r'|'.join(stripped_patterns)})"
-    else:
-        combined = rf"(?P<{groupname}>{r'|'.join(stripped_patterns)})"
-
-    return re.compile(combined)
-
-
 # Remove capture groups used for use in joining regexes in
 # multi-variants since capture groups cannot be defined more than once.
 any_event_re = combine_patterns(
@@ -131,34 +86,6 @@ any_event_tx_re = combine_patterns(
 single_variant_re = re.compile(
     rf"(?P<dna_tx>[cn]\.{any_event_tx_re.pattern})|(?P<dna>[gmo]\.{any_event_re.pattern})"
 )
-
-
-def remove_named_groups(pattern: str, noncapturing: bool = True) -> str:
-    """Function that replaces named match groups in a regular expression pattern.
-
-    Named groups are replaced with either regular parentheses or non-capturing parentheses.
-
-    Parameters
-    ----------
-    pattern : str
-        The pattern string to strip match groups from.
-
-    noncapturing : bool
-        If True, the named grouping parentheses are replaced by non-capturing parentheses.
-        If False, regular parentheses are used.
-
-    Returns
-    -------
-    str
-        The pattern string without named match groups.
-
-    """
-    if noncapturing:
-        new_parens = "(?:"
-    else:
-        new_parens = "("
-
-    return re.sub(r'\(\?P<\w+>', new_parens, pattern)
 
 
 multi_variant = rf"(?P<dna_tx_multi>[cn]\.\[{remove_named_groups(any_event_tx_re.pattern)}(?:;{remove_named_groups(any_event_tx_re.pattern)}){{1,}}\])|(?P<dna_multi>[gmo]\.\[{remove_named_groups(any_event_re.pattern)}(?:;{remove_named_groups(any_event_re.pattern)}){{1,}})\]"
---
+++ mavehgvs/patterns/util.py
@@ -0,0 +1,79 @@
+"""Utility functions for working with mavehgvs regex pattern strings.
+"""
+
+import re
+from typing import Sequence, Optional, Pattern
+
+
+def combine_patterns(
+    patterns: Sequence[str], groupname: Optional[str] = None
+) -> Pattern:
+    """Combine multiple pattern strings and generate a compiled regular expression object.
+
+    Because multiple identical group names are not allowed in a pattern, the resulting object renames all named match
+    groups such they are prefixed with the first match group name in the pattern. For example,
+    :code:`(?P<substitution>(?P<position>[1-9][0-9]*)...` becomes
+    :code:`(?P<substitution>(?P<substitution_position>[1-9][0-9]*)...`.
+
+    The function assumes that all input patterns are enclosed in parentheses.
+
+    Parameters
+    ----------
+    patterns : Sequence[str]
+        Sequence of pattern strings to combine.
+
+    groupname : Optional[str]
+        Name for the capture group surrounding the resulting pattern. If this is None, a non-capturing group will be
+        used instead.
+
+    Returns
+    -------
+    re.Pattern
+        Compiled regular expression that matches any of the input patterns. Match groups are renamed as described above
+        to attempt to ensure uniqueness across the combined pattern.
+
+    """
+    tag_re = re.compile(r"\(\?P<(\w+)>")
+    stripped_patterns = list()
+    for p in patterns:
+        tags = list(tag_re.finditer(p))
+        prefix = f"{tags[0].group(1)}_"
+        new_p = p
+        for t in tags[:0:-1]:
+            start, end = t.span(1)
+            new_p = "".join((new_p[:start], prefix, new_p[start:]))
+        stripped_patterns.append(new_p)
+    if groupname is None:
+        combined = rf"(?:{r'|'.join(stripped_patterns)})"
+    else:
+        combined = rf"(?P<{groupname}>{r'|'.join(stripped_patterns)})"
+
+    return re.compile(combined)
+
+
+def remove_named_groups(pattern: str, noncapturing: bool = True) -> str:
+    """Function that replaces named match groups in a regular expression pattern.
+
+    Named groups are replaced with either regular parentheses or non-capturing parentheses.
+
+    Parameters
+    ----------
+    pattern : str
+        The pattern string to strip match groups from.
+
+    noncapturing : bool
+        If True, the named grouping parentheses are replaced by non-capturing parentheses.
+        If False, regular parentheses are used.
+
+    Returns
+    -------
+    str
+        The pattern string without named match groups.
+
+    """
+    if noncapturing:
+        new_parens = "(?:"
+    else:
+        new_parens = "("
+
+    return re.sub(r'\(\?P<\w+>', new_parens, pattern)
