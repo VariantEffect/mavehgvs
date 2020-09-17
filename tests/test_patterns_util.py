f1d05ca3e79330358da199cd64f01d1968c87340
Alan Rubin <alan.rubin@wehi.edu.au>
2020-09-17 16:05:44 +1000

    move pattern utils

---
+++ tests/test_patterns_util.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestCombinePatterns(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+class TestRemoveNamedGroups(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
