69069f233ace8a85fe16111bfb12e90c498d0cff
Alan Rubin <alan.rubin@wehi.edu.au>
2020-09-18 18:13:33 +1000

    add rna patterns based on dna patterns

--- mavehgvs/patterns/rna.py
+++ mavehgvs/patterns/rna.py
@@ -1,91 +1,45 @@
-import re
+from fqfa.constants.iupac.rna import RNA_BASES
+from mavehgvs.patterns.util import combine_patterns, remove_named_groups
+from mavehgvs.patterns.dna import pos_cnr
 
-__all__ = [
-    "deletion_re",
-    "insertion_re",
-    "delins_re",
-    "substitution_re",
-    "multi_variant_re",
-    "single_variant_re",
-    "any_event_re",
-]
+rna_nt: str = rf"[{''.join(RNA_BASES).lower()}]"
+"""str: Pattern matching any lowercase RNA base.
 
-nucleotides = "acgubdhkmnrsvwyx"
-utr_descriptor = r"(?P<utr>[*-])"
-position = r"(?:\d+|\?)"
-interval = rf"(?:{position}_{position})"
-fragment = rf"(?:\({interval}\))"
-intronic_position = r"(?:\d+|\?|\d+(?:[\+-]?(?:\d+|\?))?)"
-intronic_interval = rf"(?:{intronic_position}_{intronic_position})"
+This does not include IUPAC ambiguity characters.
+"""
 
-# Expression with capture groups
-edge_cases = r"(?:0|spl|\?)"
-deletion = (
-    r"(?P<del>"
-    rf"(?:(?:\=(?:/|//))?(?P<interval>{interval})del)"
-    r"|"
-    rf"(?:(?P<fragment>{fragment})del)"
-    r"|"
-    rf"(?:(?P<position>{position})del(?P<base>[{nucleotides}])?)"
-    r")"
-)
-insertion = (
-    r"(?P<ins>"
-    rf"(?:(?P<interval>{interval})|(?P<fragment>{fragment}))"
-    r"ins"
-    r"(?:"
-    rf"(?P<intronic>\[(?:{intronic_interval})(?:;{intronic_interval}){{1,}}(?!;)\])"
-    r"|"
-    rf"(?:(?P<bases>[{nucleotides}]+)|(?P<length>\(\d+\)))"
-    r")"
-    r")"
-)
-delins = (
-    r"(?P<delins>"
-    r"(?:"
-    rf"(?:(?P<interval>{interval})delins)"
-    r"|"
-    rf"(?:(?P<position>{position})delins)"
-    r")"
-    rf"(?:(?P<bases>[{nucleotides}]+)|(?P<length>\(\d+\)))"
-    r")"
-)
-substitution = (
-    r"(?P<sub>"
-    r"(?:"
-    r"(?:0|\?|spl)"
-    r"|"
-    r"(?:"
-    rf"(?P<position>{position})"
-    r"(?:"
-    rf"(?:(?P<mosaic>(?:\=(?:/|//)))?(?P<ref>[{nucleotides}])>(?P<alt>[{nucleotides}]))"
-    r"|"
-    r"(?P<silent>\=)"
-    r")"
-    r")"
-    r")"
-    r")"
-)
+rna_sub: str = rf"(?P<rna_sub>(?:(?P<position>{pos_cnr})(?P<ref>{rna_nt})>(?P<alt>{rna_nt}))|(?P<equal>=))"
+"""str: Pattern matching a RNA substitution with numeric or relative-to-transcript positions.
+"""
 
+rna_del: str = rf"(?P<rna_del>(?:(?P<start>{pos_cnr})_(?P<end>{pos_cnr})del)|(?:(?P<pos>{pos_cnr})del))"
+"""str: Pattern matching a RNA deletion with numeric or relative-to-transcript positions.
+"""
 
-# Remove capture groups used for use in joining regexes in
-# multi-variants since capture groups cannot be defined more than once.
-any_event = rf"(?:{r'|'.join([insertion, deletion, delins, substitution])})"
-any_event, _ = re.subn(r"P<\w+(_\w+)?>", ":", any_event)
+rna_dup: str = rf"(?P<rna_dup>(?:(?P<start>{pos_cnr})_(?P<end>{pos_cnr})dup)|(?:(?P<pos>{pos_cnr})dup))"
+"""str: Pattern matching a RNA duplication with numeric or relative-to-transcript positions.
+"""
 
-single_variant = rf"r\.{any_event}"
-comma_separated = rf"(?:(?:{any_event})(?:,{any_event}){{1,}}(?!,))"
-semi_colon_separated = rf"(?:{any_event})(?:;{any_event}){{1,}}(?!;)"
-multi_variant = rf"r\.(?:(?:\[{semi_colon_separated}\])|(?:\[{comma_separated}\]))"
+rna_ins: str = rf"(?P<rna_ins>(?P<start>{pos_cnr})_(?P<end>{pos_cnr})ins(?P<seq>{rna_nt}+))"
+"""str: Pattern matching a RNA insertion with numeric or relative-to-transcript positions.
+"""
 
-# ---- Compiled Regexes
-deletion_re = re.compile(rf"(?:r\.)?{utr_descriptor}?{deletion}")
-insertion_re = re.compile(rf"(?:r\.)?{utr_descriptor}?{insertion}")
-delins_re = re.compile(rf"(?:r\.)?{utr_descriptor}?{delins}")
-substitution_re = re.compile(rf"(?:r\.)?{utr_descriptor}?{substitution}")
+rna_delins: str = rf"(?P<rna_delins>(?:(?P<start>{pos_cnr})_(?P<end>{pos_cnr})delins)|(?:(?P<pos>{pos_cnr})delins)(?P<seq>{rna_nt}+))"
+"""str: Pattern matching a RNA deletion-insertion with numeric or relative-to-transcript positions.
+"""
 
-single_variant_re = re.compile(single_variant)
-multi_variant_re = re.compile(multi_variant)
-comma_separated_re = re.compile(comma_separated)
-semi_colon_separated_re = re.compile(semi_colon_separated)
-any_event_re = re.compile(any_event)
+rna_variant: str = combine_patterns([rna_sub, rna_del, rna_dup, rna_ins, rna_delins], None)
+"""str: Pattern matching any single RNA variant event.
+"""
+
+rna_single_variant: str = rf"(?P<rna>r\.{rna_variant}))"
+"""str: Pattern matching any complete RNA variant, including the prefix character.
+"""
+
+rna_multi_variant: str = rf"(?P<rna_multi>r\.\[{remove_named_groups(rna_variant)}(?:;{remove_named_groups(rna_variant)}){{1,}}\])"
+"""str: Pattern matching any complete RNA multi-variant, including the prefix character.
+
+Named capture groups have been removed from the variant patterns because of non-uniqueness.
+Another applications of single-variant regular expressions is needed to recover the named groups from each individual
+variant in the multi-variant.
+"""
