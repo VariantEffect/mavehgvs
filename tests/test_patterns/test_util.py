fc1125b507563b276ead97aee26a53fb2a2a468d
Alan Rubin <alan.rubin@wehi.edu.au>
2020-09-20 23:04:57 +1000

    refactor pattern tests

--- tests/test_patterns/test_util.py
+++ tests/test_patterns/test_util.py
@@ -2,14 +2,16 @@ import unittest
 
 
 class TestCombinePatterns(unittest.TestCase):
+    @unittest.expectedFailure
     def test_something(self):
         self.assertEqual(True, False)
 
 
 class TestRemoveNamedGroups(unittest.TestCase):
+    @unittest.expectedFailure
     def test_something(self):
         self.assertEqual(True, False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
