4d9eaeae73c2ff648234b756647e70198ab7adad
Alan Rubin <alan.rubin@wehi.edu.au>
2020-09-30 15:07:15 +1000

    implement sorting and adjacency checking for positions

--- mavehgvs/position.py
+++ mavehgvs/position.py
@@ -2,7 +2,7 @@ import re
 from functools import total_ordering
 from mavehgvs.patterns.shared import pos
 
-pos_extended: str = rf"(?P<position>{pos})|(?P<position_intron>{pos}[+-]{pos})|(?P<position_utr>[*-]{pos})|(?P<position_utr_intron>[*-]{pos}[+-]{pos})"
+pos_extended: str = rf"(?P<position>[*-]?{pos})(?P<position_intron>[+-]{pos})?"
 """str: Pattern matching a position relative to a transcript with match groups.
 
 This pattern is used for sequence positions in a spliced transcript or coding sequence that are found in a UTR or
@@ -12,6 +12,28 @@ intron. It does not match integer-only positions.
 
 @total_ordering
 class VariantPosition:
+    """Class for storing a variant position.
+
+     The class includes special fields for variants using the extended position syntax.
+
+     Attributes
+     ----------
+     position : Optional[int]
+         The position as an integer.
+         Negative positions are only expected for 5' UTR positions.
+     intronic_position : Optional[int]
+         The number of bases into the intron for intronic positions.
+         None for non-intronic positions.
+
+         Nucleotides in the 5' half of the intron have positive ``intronic_position`` and their position is that of
+         the last base of the 5' exon.
+         Nucleotides in the 3' half of the intron have negative ``intronic_position`` and their position is that of
+         the first base of the 3' exon.
+     utr : Optional[bool]
+         True if the position is in the UTR. None for all other positions.
+
+     """
+
     _fullmatch = re.compile(pos_extended, flags=re.ASCII).fullmatch
     """Callable[[str, int, int], Optional[Match[str]]]: fullmatch callable for parsing positions
     
@@ -19,30 +41,12 @@ class VariantPosition:
     """
 
     def __init__(self, pos_str: str) -> None:
-        """Class for storing a variant position.
-
-        The class includes special fields for variants using the extended position syntax.
+        """Parse a position string into a VariantPosition object.
 
         Parameters
         ----------
         pos_str : str
             The string to convert to a VariantPosition object.
-
-        Attributes
-        ----------
-        position : Optional[int]
-            The integer position. None for UTR positions.
-        intronic_position : Optional[int]
-            The number of bases into the intron for intronic positions.
-            Nucleotides towards the 5' end of the intron have positive intronic_position and their position is that of
-            the last base of the 5' exon.
-            Nucleotides towards the 3' end of the intron have negative intronic_position and their position is that of
-            the first base of the 3' exon.
-            None for non-intronic positions.
-        utr : Optional[str]
-            Either "5p" or "3p" for 5' or 3' UTR positions. None for all other positions.
-        utr_position : Optional[int]
-            The number of bases into the 5' or 3' UTR for UTR positions. None for all other positions.
 
         """
         try:
@@ -53,54 +57,20 @@ class VariantPosition:
         self.position = None
         self.intronic_position = None
         self.utr = None
-        self.utr_position = None
 
         if gdict["position"] is not None:
-            self.position = int(gdict["position"])
+            if gdict["position"].startswith("*"):
+                self.utr = True
+                self.position = int(gdict["position"][1:])
+            else:
+                if gdict["position"].startswith("-"):
+                    self.utr = True
+                self.position = int(gdict["position"])
+        else:
+            raise ValueError(f"invalid variant position string '{pos_str}'")
 
-        elif gdict["position_intron"] is not None:
-            if "+" in gdict["position_intron"]:
-                self.position, self.intronic_position = (
-                    int(x) for x in gdict["position_intron"].split("+")
-                )
-            elif "-" in gdict["position_intron"]:
-                self.position, self.intronic_position = (
-                    int(x) for x in gdict["position_intron"].split("-")
-                )
-                self.intronic_position *= -1
-            else:  # pragma: no cover
-                raise ValueError("unexpected intronic position separator")
-
-        elif gdict["position_utr"] is not None:
-            if gdict["position_utr"].startswith("*"):
-                self.utr = "3p"
-            elif gdict["position_utr"].startswith("-"):
-                self.utr = "5p"
-            else:  # pragma: no cover
-                raise ValueError("unexpected UTR symbol")
-            self.utr_position = int(gdict["position_utr"][1:])
-
-        elif gdict["position_utr_intron"] is not None:
-            if gdict["position_utr_intron"].startswith("*"):
-                self.utr = "3p"
-            elif gdict["position_utr_intron"].startswith("-"):
-                self.utr = "5p"
-            else:  # pragma: no cover
-                raise ValueError("unexpected UTR symbol")
-            if "+" in gdict["position_utr_intron"][1:]:
-                self.utr_position, self.intronic_position = (
-                    int(x) for x in gdict["position_utr_intron"][1:].split("+")
-                )
-            elif "-" in gdict["position_intron"][1:]:
-                self.utr_position, self.intronic_position = (
-                    int(x) for x in gdict["position_utr_intron"][1:].split("-")
-                )
-                self.intronic_position *= -1
-            else:  # pragma: no cover
-                raise ValueError("unexpected intronic position separator")
-
-        else:  # pragma: no cover
-            raise ValueError("unexpected position format")
+        if gdict["position_intron"] is not None:
+            self.intronic_position = int(gdict["position_intron"])
 
     def is_utr(self) -> bool:
         """Return whether this is a UTR position.
@@ -111,10 +81,7 @@ class VariantPosition:
             True if the object describes a position in the UTR; else False.
 
         """
-        if self.utr is not None:
-            return True
-        else:
-            return False
+        return self.utr is not None
 
     def is_intronic(self) -> bool:
         """Return whether this is an intronic position.
@@ -125,10 +92,7 @@ class VariantPosition:
             True if the object describes a position in an intron; else False.
 
         """
-        if self.intronic_position is not None:
-            return True
-        else:
-            return False
+        return self.intronic_position is not None
 
     def is_extended(self) -> bool:
         """Return whether this position was described using the extended syntax.
@@ -145,8 +109,15 @@ class VariantPosition:
     def is_adjacent(self, other: "VariantPosition") -> bool:
         """Return whether this variant and another are immediately adjacent in sequence space.
 
-        .. note:: The special case involving the last variant in a transcript sequence and the first base in the 3'
-           UTR will be evaluated as not adjacent, as the object does not have sequence length information.
+        The following special cases are not handled correctly:
+
+        * The special case involving the last variant in a transcript sequence and the first base in the 3'
+          UTR will be evaluated as not adjacent, as the object does not have sequence length information.
+        * The special case involving the two middle bases in an intron where the numbering switches from
+          positive with respect to the 5' end of the intron to negative with respect to the 3' end of the intron
+          will be evaluated as not adjacent, as the object does not have intron length information.
+        * This ignores the special case where there is an intron between the last base of the 5' UTR and the first
+          base of the coding sequence because it is not biologically relevant to the best of my knowledge.
 
         Parameters
         ----------
@@ -159,15 +130,30 @@ class VariantPosition:
             True if the positions describe adjacent bases in sequence space; else False.
 
         """
-        # both variants describe simple positions
-        if not self.is_extended() and not other.is_extended():
-            if abs(self.position - other.position) == 1:
-                return True
+        if self.utr == other.utr:
+            if self.intronic_position is None and other.intronic_position is None:
+                return abs(self.position - other.position) == 1
+            elif (
+                self.position == other.position
+            ):  # intronic positions can only be adjacent if they are relative to the same base
+                if (
+                    self.intronic_position is not None
+                    and other.intronic_position is not None
+                ):
+                    return abs(self.intronic_position - other.intronic_position) == 1
+                else:  # handle special case for first/last base of intron and corresponding first/last base of exon
+                    return (
+                        self.intronic_position == -1
+                        or self.intronic_position == 1
+                        or other.intronic_position == -1
+                        or other.intronic_position == 1
+                    )
             else:
                 return False
-        else:
-            # TODO: implement the rest of the adjacency checks
-            pass
+        else:  # handle special case for last base of 5' utr and first base of non-UTR sequence
+            return (self.position == -1 and other.position == 1) or (
+                other.position == -1 and self.position == 1
+            )
 
     def __lt__(self, other: "VariantPosition") -> bool:
         """Less than comparison operator.
@@ -185,49 +171,27 @@ class VariantPosition:
             True if this position evaluates as strictly less than the other position; else False.
 
         """
-
-        def intron_lt(a: "VariantPosition", b: "VariantPosition") -> bool:
-            """Compare two positions based on their intronic positions assuming the positions are otherwise equal.
-
-            Parameters
-            ----------
-            a : VariantPosition
-                The first position to compare.
-            b : VariantPosition
-                The second position to compare.
-
-            Returns
-            -------
-            bool
-                True if a is strictly less than b based on the intronic position, assuming that all other fields are
-                equal.
-
-            """
-            if a.intronic_position == b.intronic_position:  # variants must be equal
-                return False
-            elif a.intronic_position is None:
-                return b.intronic_position < 0
-            elif b.intronic_position is None:
-                return a.intronic_position < 0
-            else:
-                return a.intronic_position < b.intronic_position
-
         if self.utr == other.utr:
-            if self.utr is not None:
-                if self.utr_position != other.utr_position:
-                    return self.utr_position < other.utr_position
-                else:
-                    return intron_lt(self, other)
-            else:  # both are not UTR positions
-                if self.position != other.position:
-                    return self.position < other.position
-                else:
-                    return intron_lt(self, other)
-        else:  # 5p < non-UTR < 3p
-            if self.utr == "5p" or other.utr == "3p":
-                return True
-            elif self.utr == "3p" or other.utr == "5p":
-                return False
+            if self.position == other.position:
+                if self.intronic_position == other.intronic_position:
+                    return False
+                elif self.intronic_position is None:
+                    return other.intronic_position < 0
+                elif other.intronic_position is None:
+                    return self.intronic_position > 0
+            else:
+                return self.position < other.position
+        else:  # 5' < non-UTR < 3'
+            if self.utr:
+                if self.position < 0:  # self is in 5' UTR
+                    return True
+                else:  # self is in 3' UTR
+                    return False
+            else:
+                if other.position < 0:  # other is in 5' UTR
+                    return False
+                else:  # other is in 3' UTR
+                    return True
 
     def __eq__(self, other: "VariantPosition") -> bool:
         """Equality comparison operator.
@@ -245,11 +209,10 @@ class VariantPosition:
             True if this position is the same as the other position; else False.
 
         """
-        return (self.position, self.intronic_position, self.utr, self.utr_position) == (
+        return (self.position, self.intronic_position, self.utr) == (
             other.position,
             other.intronic_position,
             other.utr,
-            other.utr_position,
         )
 
     def __ne__(self, other: "VariantPosition") -> bool:
@@ -268,9 +231,8 @@ class VariantPosition:
             True if this position is not the same as the other position; else False.
 
         """
-        return (self.position, self.intronic_position, self.utr, self.utr_position) != (
+        return (self.position, self.intronic_position, self.utr) != (
             other.position,
             other.intronic_position,
             other.utr,
-            other.utr_position,
         )
