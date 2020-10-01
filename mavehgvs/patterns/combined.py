28297f043453376ed7de5514968d7ead335ba3d7
Alan Rubin <alan.rubin@wehi.edu.au>
2020-10-01 14:25:54 +1000

    add combined pattern that matches any valid variant

---
+++ mavehgvs/patterns/combined.py
@@ -0,0 +1,5 @@
+from mavehgvs.patterns.dna import dna_single_variant, dna_multi_variant
+from mavehgvs.patterns.rna import rna_single_variant, rna_multi_variant
+from mavehgvs.patterns.protein import pro_single_variant, pro_multi_variant
+
+any_variant = rf"(?:(?P<reference_id>\w+):)?(?P<single_variant>{r'|'.join([dna_single_variant, rna_single_variant, pro_single_variant])})|(?P<multi_variant>{r'|'.join([dna_multi_variant, rna_multi_variant, pro_multi_variant])})"
