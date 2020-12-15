7677ba1256dae55b3f66c587766cedaffb8f512b
Alan Rubin <alan.rubin@wehi.edu.au>
2020-12-15 14:35:48 +1100

    add docstring and use except/else

--- mavehgvs/utilities.py
+++ mavehgvs/utilities.py
@@ -9,15 +9,34 @@ __all__ = ["parse_variant_strings"]
 def parse_variant_strings(
     variants: List[str]
 ) -> Tuple[List[Optional[Variant]], List[Optional[str]]]:
-    valid, invalid = [], []
+    """Parse a list of MAVE-HGVS strings into Variant objects or error messages.
 
-    for variant in variants:
+    Parameters
+    ----------
+    variants : List[str]
+        List of MAVE-HGVS strings to parse.
+
+    Returns
+    -------
+    Tuple[List[Optional[Variant]], List[Optional[str]]]
+        Returns a pair of lists containing variants or error messages.
+
+        Both lists have the same length as the input list.
+        The first list contains Variant objects if the string was successfully parsed; else None.
+        The second list contains None if the string was successfully parsed; else the error message.
+
+    """
+    valid = list()
+    invalid = list()
+
+    for s in variants:
         try:
-            v = Variant(variant)
-            valid.append(v)
-            invalid.append(None)
+            v = Variant(s)
         except MaveHgvsParseError as error:
             valid.append(None)
             invalid.append(str(error))
+        else:
+            valid.append(v)
+            invalid.append(None)
 
     return valid, invalid
