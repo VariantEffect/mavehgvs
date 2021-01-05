ab1e5e958df354a1ea0c9e340352447ad4debc7d
Alan Rubin <alan.rubin@wehi.edu.au>
2021-01-05 19:01:44 +1100

    update fqfa version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-requirements = ["fqfa>=1.2.0"]
+requirements = ["fqfa>=1.2.1"]
 # fqfa requires backported dataclasses in Python 3.6
 if sys.version_info.major == 3 and sys.version_info.minor == 6:
     requirements.append("dataclasses")
